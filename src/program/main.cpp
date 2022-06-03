b4e958fa13a1f6eb3e9f1029e574d2f0544ca387
fruityloops1 <fruityloops@littun.co>
2022-06-03 18:41:36 +0200

    remove unneeded uintptr_t cast

--- src/program/main.cpp
+++ src/program/main.cpp
@@ -7,7 +7,7 @@ extern "C" void exl_main(void* x0, void* x1) {
     envSetOwnProcessHandle(exl::util::proc_handle::Get());
     exl::hook::Initialize();
 
-    INJECT_HOOK((uintptr_t)0x003d86b0, actorFactory);
+    INJECT_HOOK(0x003d86b0, actorFactory);
 }
 
 extern "C" NORETURN void exl_exception_entry() {
