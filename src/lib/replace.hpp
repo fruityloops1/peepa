c2928b2cbd7a7c629b9f5de7a3454b584151686b
fruityloops1 <fruityloops@littun.co>
2022-06-07 23:31:01 +0200

    add REPLACE_DATA macro (oops)

--- src/lib/replace.hpp
+++ src/lib/replace.hpp
@@ -9,6 +9,7 @@ namespace exl::replace {
         "__asm" #name ":\n" assembly);                                                                                 \
     extern const u32 __asm##name[]
 
+#define REPLACE_DATA(target, source, size) exl::replace::ReplaceData((uintptr_t)target, (uintptr_t)source, size)
 #define REPLACE_ASM(target, name, length) REPLACE_DATA(target, __asm##name, length * sizeof(u32))
 #define REPLACE_ASM_O(offset, name, length) REPLACE_ASM(exl::hook::GetTargetOffset(offset), name, length)
 } // namespace exl::replace
