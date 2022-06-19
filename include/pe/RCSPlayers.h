3e06d89fcd693b98148e692184ce26b8031088b3
fruityloops1 <fruityloops@littun.co>
2022-06-19 23:49:10 +0200

    add non-mario characters in BF

---
+++ include/pe/RCSPlayers.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace pe {
+
+void initRCSPlayerHooks();
+
+} // namespace
---
+++ src/program/pe/RCSPlayers.cpp
@@ -0,0 +1,34 @@
+#include "al/util/NerveUtil.h"
+#include "lib.hpp"
+#include "pe/RCSPlayers.h"
+#include "al/actor/LiveActor.h"
+
+MAKE_ASM(fightInitFix, R"(
+    nop
+    nop
+    nop
+    nop
+)");
+
+MAKE_ASM(secondPhaseBowserDefeatCrashFix, "nop");
+
+template <uintptr_t nerveOffset>
+static void setNerveHook(al::LiveActor* actor)
+{
+    al::Nerve* nerve = (al::Nerve*)exl::hook::GetTargetOffset(nerveOffset);
+    al::setNerve(actor, nerve);
+}
+
+static const char* const getPlayerNameHook()
+{
+    return "Peach"; /* Mario, Luigi, Peach, Kinopio, Rosetta */
+}
+
+void pe::initRCSPlayerHooks() {
+    exl::patch::CodePatcher(0x0040f660).BranchInst(getPlayerNameHook);
+    REPLACE_ASM_O(0x0039f580, fightInitFix, 4);
+    exl::patch::CodePatcher(0x0039f59c).BranchInst(al::setNerve);
+    exl::patch::CodePatcher(0x003a67d0).BranchInst(setNerveHook<0x0137f608>);
+    exl::patch::CodePatcher(0x003a7ef4).BranchInst(setNerveHook<0x0137f630>);
+    REPLACE_ASM_O(0x00872c68, secondPhaseBowserDefeatCrashFix, 1);
+}
